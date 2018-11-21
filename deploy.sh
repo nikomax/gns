73773964f03c6bcb69ae86cb1e9debc95eefd5a2
maks <maks@coderiver.com.ua>
2018-11-21 18:52:09 +0200

    Build

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+rm -rf ./dist
+npm run build
+cd dist
+cp index.html 404.html
+git init
+git remote add origin https://github.com/nikomax-gns/nikomax-gns.github.io.git
+git add -A
+git commit -m "Build"
+git push -f origin master
